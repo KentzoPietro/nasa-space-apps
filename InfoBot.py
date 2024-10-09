8a2ba9fbd156dfb2f533ba92a0c2ad48dbef56b4
kenzo <kentzopietro@gmail.com>
2024-10-09 20:31:53 +0700

    remove API keys

--- InfoBot.py
+++ InfoBot.py
@@ -7,11 +7,10 @@ from llama_index.embeddings.openai import OpenAIEmbedding
 from dotenv import load_dotenv
 
 load_dotenv()
-Settings.embed_model = OpenAIEmbedding(api_key='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')
-GROQ_API_KEY = os.getenv("GROQ_API_KEY")
-def infoBot(prompt):
-    Settings.llm = Groq(model="llama3-70b-8192", api_key="GROQ_API_KEY")
+Settings.embed_model = OpenAIEmbedding(api_key=os.getenv('OPENAI_API_KEY'))
+Settings.llm = Groq(model="llama3-70b-8192", api_key=os.getenv("GROQ_API_KEY"))
 
+def infoBot(prompt):
     # Read documents from the reader
     print("Loading documents...")
     
